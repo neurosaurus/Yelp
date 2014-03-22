654538c1a15177cfdec61e451df3c690758b030b
Tripta Gupta <gupta.tripta@gmail.com>
2014-03-22 13:31:55 -0700

    add pods

---
+++ Yelp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Yelp
+//
+//  Created by Tripta Gupta on 3/22/14.
+//  Copyright (c) 2014 Tripta Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YelpTests/YelpTests.m
@@ -0,0 +1,34 @@
+//
+//  YelpTests.m
+//  YelpTests
+//
+//  Created by Tripta Gupta on 3/22/14.
+//  Copyright (c) 2014 Tripta Gupta. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YelpTests : XCTestCase
+
+@end
+
+@implementation YelpTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
